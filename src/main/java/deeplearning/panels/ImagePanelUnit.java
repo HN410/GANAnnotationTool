7544ff6c79fdc788ae2f27665a54529393c87bf3
hn410 <sirrook7@gmail.com>
2021-08-15 20:22:54 +0900

    add getResizedImageIcon

--- src/main/java/deeplearning/panels/ImagePanelUnit.java
+++ src/main/java/deeplearning/panels/ImagePanelUnit.java
@@ -2,6 +2,8 @@ package deeplearning.panels;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.MediaTracker;
+import java.awt.Image;
 
 import javax.swing.Box;
 import javax.swing.ImageIcon;
@@ -10,6 +12,7 @@ import javax.swing.JPanel;
 
 //画像一枚分のJPanel
 public class ImagePanelUnit extends JPanel{
+    private static int IMAGE_SIZE = 300;
     private static int LABEL_MARGIN_W = 10;
 
     private JLabel imageLabel;
@@ -27,12 +30,13 @@ public class ImagePanelUnit extends JPanel{
     }
 
     private void setImage() {
-        ImageIcon icon = new ImageIcon("./test.jpg");
+        ImageIcon icon = getResizedImageIcon("./test.jpg");
         imageLabel.setIcon(icon);
     }
 
     private JLabel getImageLabel() {
         JLabel label = new JLabel();
+        label.setPreferredSize(new Dimension(IMAGE_SIZE, IMAGE_SIZE));
 
         return label;
     }
@@ -50,5 +54,26 @@ public class ImagePanelUnit extends JPanel{
 
         return panel;
     }
+ 
     
+    private ImageIcon getResizedImageIcon(String imagePath){
+        MediaTracker tracker = new MediaTracker(this); 
+
+        ImageIcon icon = new ImageIcon(imagePath);
+        int height = icon.getIconHeight();
+        int width = icon.getIconWidth();
+        Image image = icon.getImage();
+        if(height > width){
+            image = image.getScaledInstance(-1, IMAGE_SIZE, Image.SCALE_SMOOTH);
+        }else{
+            image = image.getScaledInstance(IMAGE_SIZE, -1, Image.SCALE_SMOOTH);
+        }
+
+        
+        
+        tracker.addImage(image, 1); //縮小処理終了まで待機
+        icon = new ImageIcon(image);
+
+        return icon;
+    }
 }
