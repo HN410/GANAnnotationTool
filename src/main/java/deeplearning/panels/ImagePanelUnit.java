36e1cf6eb4bb74c4b62d1df7f0cb362b62c3ec78
hn410 <sirrook7@gmail.com>
2021-08-15 18:43:02 +0900

    add label margin

--- src/main/java/deeplearning/panels/ImagePanelUnit.java
+++ src/main/java/deeplearning/panels/ImagePanelUnit.java
@@ -1,19 +1,36 @@
 package deeplearning.panels;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.Box;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 //画像一枚分のJPanel
 public class ImagePanelUnit extends JPanel{
+    private static int LABEL_MARGIN_W = 10;
 
     public ImagePanelUnit(String labelText){
+        setLayout(new BorderLayout());
+
+        JPanel label = getLabelPanel(labelText);
+        add(BorderLayout.SOUTH, label);
+
+    }
+
+    private JPanel getLabelPanel(String labelText){
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
 
         JLabel label = new JLabel(labelText);
-        panel.add(BorderLayout.SOUTH, label);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        panel.add(BorderLayout.CENTER, label);
 
+        panel.add(BorderLayout.SOUTH,
+             Box.createRigidArea(new Dimension(LABEL_MARGIN_W, LABEL_MARGIN_W)));
+
+        return panel;
     }
     
 }
