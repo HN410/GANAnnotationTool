d8d156da4ec4518cbf6e4891713a9b0eda243c50
hn410 <sirrook7@gmail.com>
2021-08-15 20:12:14 +0900

    add setImage

--- src/main/java/deeplearning/panels/ImagePanelUnit.java
+++ src/main/java/deeplearning/panels/ImagePanelUnit.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 import javax.swing.Box;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -11,12 +12,29 @@ import javax.swing.JPanel;
 public class ImagePanelUnit extends JPanel{
     private static int LABEL_MARGIN_W = 10;
 
+    private JLabel imageLabel;
+
     public ImagePanelUnit(String labelText){
         setLayout(new BorderLayout());
 
+        imageLabel = getImageLabel();
+        setImage();
+
         JPanel label = getLabelPanel(labelText);
         add(BorderLayout.SOUTH, label);
+        add(BorderLayout.CENTER, imageLabel);
 
+    }
+
+    private void setImage() {
+        ImageIcon icon = new ImageIcon("./test.jpg");
+        imageLabel.setIcon(icon);
+    }
+
+    private JLabel getImageLabel() {
+        JLabel label = new JLabel();
+
+        return label;
     }
 
     private JPanel getLabelPanel(String labelText){
