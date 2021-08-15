5e1c57765ceeedebda6744627e51323084e94d65
hn410 <sirrook7@gmail.com>
2021-08-15 17:42:33 +0900

    init

---
+++ src/main/java/deeplearning/panels/ImagesPanel.java
@@ -0,0 +1,14 @@
+package deeplearning.panels;
+
+import javax.swing.JPanel;
+
+import javafx.scene.image.Image;
+
+public class ImagesPanel extends JPanel{
+    public ImagesPanel(){
+        super();
+        
+
+    }
+    
+}
