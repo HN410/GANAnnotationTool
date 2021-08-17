16c3f5b9d8e89963b21828f9e57a52f53022da86
hn410 <sirrook7@gmail.com>
2021-08-17 14:34:01 +0900

    add saveMenu

--- src/main/java/deeplearning/menu/MenuBar.java
+++ src/main/java/deeplearning/menu/MenuBar.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 import deeplearning.main.MainWindow;
 import deeplearning.main.PropertiesClass;
@@ -14,10 +15,15 @@ public class MenuBar extends JMenuBar{
     private static final String FILE_MENU_NAME = "ファイル";
     private static final String OPTION_MENU_NAME = "オプション";
     private static final String ORIGIN_REMOVE_CHECK = "元画像を削除";
+    private static final String SAVE = "保存";
+
+    private static final String SAVE_COMPLETED = "保存に成功しました";
     
     private MainWindow mainwWindow;
 
+    private JMenuItem saveMenu;
     public JCheckBoxMenuItem originRemoveCheck;
+    
     public MenuBar(MainWindow mainWindow){
         super();
         this.mainwWindow = mainWindow;
@@ -29,8 +35,26 @@ public class MenuBar extends JMenuBar{
 
         originRemoveCheck = new JCheckBoxMenuItem(ORIGIN_REMOVE_CHECK);
         initOriginRemoveCheck();
+        
+        saveMenu = getSaveMenu();
+
+        fileMenu.add(saveMenu);
         optionMenu.add(originRemoveCheck);        
+
     }
+    private JMenuItem getSaveMenu() {
+        JMenuItem item = new JMenuItem(SAVE);
+        item.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mainwWindow.dataSave();
+                mainwWindow.setMessage(SAVE_COMPLETED);
+            }
+        });
+        return item;
+    }
+
+
     private void initOriginRemoveCheck() {
         boolean value = mainwWindow.properties.getProperty(PropertiesClass.IMAGE_REMOVE)
             .equals(PropertiesClass.TRUE);
